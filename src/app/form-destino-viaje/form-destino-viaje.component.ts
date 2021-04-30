1584785731fd277cd3d7899254037e04ff06ee70
Paul Reinoso <washington.reinoso@epn.edu.ec>
2021-04-30 00:36:25 -0500

    Validaciones personalizadas CMÑ

--- src/app/form-destino-viaje/form-destino-viaje.component.ts
+++ src/app/form-destino-viaje/form-destino-viaje.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { DestinoVije } from '../models/destino-viaje.model';
-import { FormGroup, FormBuilder } from '@angular/forms';
-
-
+import { FormGroup, FormBuilder, Validators, FormControl, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-form-destino-viaje',
@@ -11,15 +9,15 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 })
 export class FormDestinoViajeComponent implements OnInit {
 
-
-
   @Output() onItemAdded!: EventEmitter<DestinoVije>;
   fg!: FormGroup;
+  maxLongitudNombre:number=10;
 
   constructor(fb: FormBuilder) { //fb nos permite definir o construir el formulario
     this.onItemAdded = new EventEmitter();
     this.fg = fb.group({//pasamos un objeto de inicializacion que define estructur del formulario controles  vincular
-      nombre:  fb.control('',[]),//control para vincular al tag html del nombre -> nombre: fb.control(''/*valor*/,[]/*validadores*/)
+      //nombre:  fb.control('',[Validators.required]),//control para vincular al tag html del nombre -> nombre: fb.control(''/*valor*/,[]/*validadores*/)
+      nombre:  fb.control('',Validators.compose([Validators.required,this.nombreValidador,Validators.maxLength(this.maxLongitudNombre)])),//
       url:  fb.control('',[]),
       descripcion:  fb.control('',[]),
     });
@@ -44,9 +42,35 @@ export class FormDestinoViajeComponent implements OnInit {
     return false;
   }
 
-
-
   ngOnInit(): void {
   }
+
+  nombreValidador(control:FormControl): Object | null{ //recive un control  en el que se implementa la validcion y devolvemos un objeto key y un valor boolean
+    //{[s:string]:boolean}  //Object
+    const longitud= control.value.toString().trim().length;
+    if(longitud > 0 && longitud < 5){
+      return {'invalidNombre':true}
+    }
+    return null;
+  }
+  //Funcion ejemplo para crear tu propio validador 
+  nombreValidatorParametrizable(minLong: number): ValidatorFn {
+    (control: FormControl): Object | null => {
+    //{ [key: string]: boolean }
+    let l = control.value.toString().trim().length;
+      if (l > 0 && l < minLong) {
+            return { 'minLongNombre': true };
+        }
+        return null;
+    }
+    return Object;
+}
+
+
+
+
+
+
+
 
 }
